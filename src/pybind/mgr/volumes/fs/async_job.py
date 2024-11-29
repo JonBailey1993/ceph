ceph/ceph-ci

mgr/vol: reuse code for spawning threads
Signed-off-by: Rishabh Dave <ridave@redhat.com>

src/pybind/mgr/volumes/fs/async_job.py
@@ -137,9 +137,16 @@ class AsyncJobs(threading.Thread):
 
         self.threads = []
         for i in range(self.nr_concurrent_jobs):
-            self.threads.append(JobThread(self, volume_client, name="{0}.{1}".format(self.name_pfx, i)))
-            self.threads[-1].start()
+            self.spawn_new_thread(i)
         self.start()
+
+    def spawn_new_thread(self, suffix):
+        t_name = f'{self.name_pfx}.{time.time()}.{suffix}'
+        log.info(f'spawning new thread with name {t_name}')
+        t = JobThread(self, self.vc, name=t_name)
+        t.start()
+
+        self.threads.append(t)
 
     def set_wakeup_timeout(self):
         with self.lock:
@@ -165,8 +172,7 @@ class AsyncJobs(threading.Thread):
                     # Increase concurrency: create more threads.
                     log.debug("creating new threads to job increase")
                     for i in range(c, self.nr_concurrent_jobs):
-                        self.threads.append(JobThread(self, self.vc, name="{0}.{1}.{2}".format(self.name_pfx, time.time(), i)))
-                        self.threads[-1].start()
+                        self.spawn_new_thread(i)
                 self.cv.wait(timeout=5)
 
     def shutdown(self):

JonBailey1993/ceph

src/pybind/mgr/volumes/fs/async_job.py
@@ -143,9 +143,16 @@ class AsyncJobs(threading.Thread):
 
         self.threads = []
         for i in range(self.nr_concurrent_jobs):
-            self.threads.append(JobThread(self, volume_client, name="{0}.{1}".format(self.name_pfx, i)))
-            self.threads[-1].start()
+            self.spawn_new_thread(i)
         self.start()
+
+    def spawn_new_thread(self, suffix):
+        t_name = f'{self.name_pfx}.{time.time()}.{suffix}'
+        log.debug(f'spawning new thread with name {t_name}')
+        t = JobThread(self, self.vc, name=t_name)
+        t.start()
+
+        self.threads.append(t)
 
     def set_wakeup_timeout(self):
         with self.lock:
@@ -171,8 +178,7 @@ class AsyncJobs(threading.Thread):
                     # Increase concurrency: create more threads.
                     log.debug("creating new threads to job increase")
                     for i in range(c, self.nr_concurrent_jobs):
-                        self.threads.append(JobThread(self, self.vc, name="{0}.{1}.{2}".format(self.name_pfx, time.time(), i)))
-                        self.threads[-1].start()
+                        self.spawn_new_thread(i)
                 self.cv.wait(timeout=self.wakeup_timeout)
 
     def shutdown(self):